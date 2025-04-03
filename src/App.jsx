3d02392a1fcf822f84860c4e940017fcad2c39aa
ahmedhat <2023170001@cis.asu.edu.eg>
2025-04-03 21:02:53 +0200

    react initial folders added and the entire project folder structure created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+﻿import React from 'react';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return (
+    <div className='App'>
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
